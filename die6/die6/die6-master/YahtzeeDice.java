1bfb2f0dd41d046e2639c5b9e64bc46fb2b78087
Rohan2026 <143441935+Rohan2026@users.noreply.github.com>
2023-12-04 11:50:46 -0800

    Add files via upload

---
+++ die6/die6/die6-master/YahtzeeDice.java
@@ -0,0 +1,31 @@
+
+public class YahtzeeDice
+{
+
+    Die6 dieA;
+    Die6 dieB;
+    Die6 dieC;
+    Die6 dieD;
+    Die6 dieE;
+    public YahtzeeDice()
+    {
+        dieA = new Die6();
+    }
+    
+    public void roll()
+    {
+    }  
+    
+    public String summarize()
+    {    
+    }
+    
+    public String toString()
+    {
+    }
+
+    public String rollAndSummarize()
+    {
+    }
+    
+}

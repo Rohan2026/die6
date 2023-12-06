6c773392b2020b08ca5b1093caa3222463495548
Rohan Huynh <rohan.huynh@mrjaffesclass.com>
2023-12-06 11:21:23 -0800

    12/6/23

--- die6/die6/die6-master/YahtzeeDice.java
+++ die6/die6/die6-master/YahtzeeDice.java
@@ -2,30 +2,47 @@
 public class YahtzeeDice
 {
 
-    Die6 dieA;
-    Die6 dieB;
-    Die6 dieC;
-    Die6 dieD;
-    Die6 dieE;
+    Die6 die1;
+    Die6 die2;
+    Die6 die3;
+    Die6 die4;
+    Die6 die5;
     public YahtzeeDice()
     {
-        dieA = new Die6();
+        die1 = new Die6();
+        die2 = new Die6();
+        die3 = new Die6();
+        die4 = new Die6();
+        die5 = new Die6();
     }
     
     public void roll()
     {
+        die1.roll();
+        die2.roll();
+        die3.roll();
+        die4.roll();
+        die5.roll();
     }  
     
+    public void roll(int dieNumber)
+    {
+        
+    }
+    
     public String summarize()
-    {    
+    {
+        return "hello";
     }
     
     public String toString()
     {
+        return "hello";
     }
 
     public String rollAndSummarize()
     {
+        return "Hello";
     }
     
 }
